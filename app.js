e578474ac092983ca06be3e611bb70f871fb674e
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-06-01 02:55:32 +0100

    Feature:(set data to redis and get jobs endpoint)

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express =  require('express')
-
 const app = express()
+const redis = require("redis");
+const client = redis.createClient();
+const { promisify } = require("util");
+
+const getAsync = promisify(client.get).bind(client);
 
 // app.use('/',(req, res)=>{
 //     return res.status(200).json({
 //         "message" : "express dey work"
 //     })
 // })
+
+
+app.get('/jobs/remote',async (req, res, next) => {
+
+const jobs = await getAsync('remotejobs');
+res.status(200).send(jobs)
+})
 
 const PORT = process.env.PORT || 3200; 
 
--- server/cronjobs/index.js
+++ server/cronjobs/index.js
@@ -1,5 +1,7 @@
 var CronJob = require('cron').CronJob;
-var job = new CronJob('* * * * * *', function() {
-  console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
+
+const remoteJobs = require('../api/index');
+
+// Refetch on sundays
+var job = new CronJob('0 0 * * 0', remoteJobs , null, true, 'America/Los_Angeles');
 job.start();
